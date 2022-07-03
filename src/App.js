7c063889cb64ad86bb1052b9f69e8b7354872c80
Lucas Vieira <vieira.lucasmm@gmail.com>
2022-07-03 00:31:24 -0300

    add useState

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import BookList from "./components/BookList";
-import Navbar from "./components/Navbar";
-import ThemeToggle from "./components/ThemeToggle";
-import AuthContextProvider from "./context/AuthContext";
-import ThemeContextProvider from "./context/ThemeContext";
+// import BookList from "./components/BookList";
+// import Navbar from "./components/Navbar";
+// import ThemeToggle from "./components/ThemeToggle";
+// import AuthContextProvider from "./context/AuthContext";
+// import ThemeContextProvider from "./context/ThemeContext";
+
+import SongList from "./components/SongList";
 
 function App() {
   return (
     <div className="App">
-      <ThemeContextProvider>
+      <SongList/>
+      {/* <ThemeContextProvider>
         <AuthContextProvider>
           <Navbar/>
           <BookList/>
           <ThemeToggle/>
         </AuthContextProvider>
-      </ThemeContextProvider>
+      </ThemeContextProvider> */}
     </div>
   );
 }
